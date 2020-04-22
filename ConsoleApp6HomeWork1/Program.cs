1c24a7f2386c553018eb9d51991da8de8e49b06f
KotyhovaAnna <anna.kotihova@mail.ru>
2020-04-22 23:39:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6HomeWork1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+
+	static void Main(string[] args)
+	{
+		int[] Array = { 1, 2, 7, 19, 24, 33, 10, 48 };
+
+		int result = 0;
+		for (int i = 0; i < Array.Length; i++)
+
+			if (Array[i] % 2 == 0)
+			{
+				result += Array[i];
+			}
+
+		Console.WriteLine(result);
+
+		Console.ReadLine();
+	}
+}
